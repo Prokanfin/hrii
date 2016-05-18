07e46c8b1ee4130e6a882b27d3f21bb33c10bd5a
thesis-cpe <witthayaniphon@hotmail.com>
2016-05-18 11:37:18 +0700

    ตัวอย่าง

--- apps/resources/views/dashboard.blade.php
+++ apps/resources/views/dashboard.blade.php
@@ -1,30 +1,43 @@
 
-  <!--[if lt IE 8]>
-        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
-  
+<!--[if lt IE 8]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+
 <!--navbar-->  
-  @extends('layouts.master')
-  @section('navbar')
+@extends('layouts.master')
+@section('navbar')
 
- <!--/navbar--> 
- 
- <!--sidebar-->
-  @section('sidebar')
-  <!--/sidebar-->
- 
-  
-  <!--grid menu-->
+<!--/navbar--> 
 
-   <!--/grid menu-->
-  
-  <!-- content -->
-  @section('content')
-  <p>This is my body content222.</p>
-  @endsection
-  
- <!-- /Page -->
-  <!-- End Page -->
-  <!-- Footer -->
-    @section('footer')
- 
+<!--sidebar-->
+@section('sidebar')
+<!--/sidebar-->
+
+
+<!--grid menu-->
+
+<!--/grid menu-->
+
+<!-- content -->
+@section('content')
+<div class="row">
+    <!--Calendar-->
+    <div class="col-sm-8">
+        Calendar
+    </div> <!--/Calendar-->
+    <!--กล่อง 4 กล่อง-->
+    <div class="4">
+        กล่อง 4 กล่อง
+       @if(Request::is('dashboard'))
+         hello
+       @endif
+        
+    </div>
+</div>   
+@endsection
+
+<!-- /Page -->
+<!-- End Page -->
+<!-- Footer -->
+@section('footer')
+
